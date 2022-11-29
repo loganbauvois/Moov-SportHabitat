d8c9803c2c2da819921eca5add29e6940787136b
loganbauvois <74671579+loganbauvois@users.noreply.github.com>
2022-11-29 11:16:13 +0100

    Add files via upload

---
+++ Site/script.js
@@ -0,0 +1,123 @@
+function AfficherMuscu(){
+    var muscu = document.getElementById("muscu").style.display = "block";
+    var yoga = document.getElementById("yoga").style.display = "none";
+    var ping = document.getElementById("ping").style.display = "none";
+    var vest = document.getElementById("vest").style.display = "none";
+    var muscu2 = document.getElementById("truc1").style.opacity = 1;
+    var muscu2 = document.getElementById("truc2").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc3").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc4").style.opacity = 0.5;
+}
+function AfficherYoga() {
+    var muscu = document.getElementById("muscu").style.display = "none";
+    var yoga = document.getElementById("yoga").style.display = "block";
+    var ping = document.getElementById("ping").style.display = "none";
+    var vest = document.getElementById("vest").style.display = "none";
+    var muscu2 = document.getElementById("truc1").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc2").style.opacity = 1;
+    var muscu2 = document.getElementById("truc3").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc4").style.opacity = 0.5;
+}
+function AfficherPing() {
+    var muscu = document.getElementById("muscu").style.display = "none";
+    var yoga = document.getElementById("yoga").style.display = "none";
+    var ping = document.getElementById("ping").style.display = "block";
+    var vest = document.getElementById("vest").style.display = "none";
+    var muscu2 = document.getElementById("truc1").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc2").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc3").style.opacity = 1;
+    var muscu2 = document.getElementById("truc4").style.opacity = 0.5;
+}
+function AfficherVest() {
+    var muscu = document.getElementById("muscu").style.display = "none";
+    var yoga = document.getElementById("yoga").style.display = "none";
+    var ping = document.getElementById("ping").style.display = "none";
+    var vest = document.getElementById("vest").style.display = "block";
+    var muscu2 = document.getElementById("truc1").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc2").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc3").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc4").style.opacity = 1;
+}
+
+function AfficherVenteMuscu() {
+    var ventemuscu = document.getElementById("ventemuscu").style.display = "block";
+    var locationmuscu = document.getElementById("locationmuscu").style.display = "none";
+    var venteyoga = document.getElementById("venteyoga").style.display = "none";
+    var locationyoga = document.getElementById("locationyoga").style.display = "none";
+    var venteping = document.getElementById("venteping").style.display = "none";
+    var locationping = document.getElementById("locationping").style.display = "none";
+    var locationvest = document.getElementById("locationvest").style.display = "none";
+    var muscu2 = document.getElementById("truc5").style.opacity = 1;
+    var muscu2 = document.getElementById("truc6").style.opacity = 0.5;
+}
+
+function AfficherLocationMuscu() {
+    var ventemuscu = document.getElementById("ventemuscu").style.display = "none";
+    var locationmuscu = document.getElementById("locationmuscu").style.display = "block";
+    var venteyoga = document.getElementById("venteyoga").style.display = "none";
+    var locationyoga = document.getElementById("locationyoga").style.display = "none";
+    var venteping = document.getElementById("venteping").style.display = "none";
+    var locationping = document.getElementById("locationping").style.display = "none";
+    var locationvest = document.getElementById("locationvest").style.display = "none";
+    var muscu2 = document.getElementById("truc5").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc6").style.opacity = 1;
+}
+function AfficherVenteYoga() {
+    var ventemuscu = document.getElementById("ventemuscu").style.display = "none";
+    var locationmuscu = document.getElementById("locationmuscu").style.display = "none";
+    var venteyoga = document.getElementById("venteyoga").style.display = "block";
+    var locationyoga = document.getElementById("locationyoga").style.display = "none";
+    var venteping = document.getElementById("venteping").style.display = "none";
+    var locationping = document.getElementById("locationping").style.display = "none";
+    var locationvest = document.getElementById("locationvest").style.display = "none";
+    var muscu2 = document.getElementById("truc7").style.opacity = 1;
+    var muscu2 = document.getElementById("truc8").style.opacity = 0.5;
+}
+
+function AfficherLocationYoga() {
+    var ventemuscu = document.getElementById("ventemuscu").style.display = "none";
+    var locationmuscu = document.getElementById("locationmuscu").style.display = "none";
+    var venteyoga = document.getElementById("venteyoga").style.display = "none";
+    var locationyoga = document.getElementById("locationyoga").style.display = "block";
+    var venteping = document.getElementById("venteping").style.display = "none";
+    var locationping = document.getElementById("locationping").style.display = "none";
+    var locationvest = document.getElementById("locationvest").style.display = "none";
+    var muscu2 = document.getElementById("truc7").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc8").style.opacity = 1;
+}
+
+function AfficherVentePing() {
+    var ventemuscu = document.getElementById("ventemuscu").style.display = "none";
+    var locationmuscu = document.getElementById("locationmuscu").style.display = "none";
+    var venteyoga = document.getElementById("venteyoga").style.display = "none";
+    var locationyoga = document.getElementById("locationyoga").style.display = "none";
+    var venteping = document.getElementById("venteping").style.display = "block";
+    var locationping = document.getElementById("locationping").style.display = "none";
+    var locationvest = document.getElementById("locationvest").style.display = "none";
+    var muscu2 = document.getElementById("truc9").style.opacity = 1;
+    var muscu2 = document.getElementById("truc10").style.opacity = 0.5;
+}
+
+function AfficherLocationPing() {
+    var ventemuscu = document.getElementById("ventemuscu").style.display = "none";
+    var locationmuscu = document.getElementById("locationmuscu").style.display = "none";
+    var venteyoga = document.getElementById("venteyoga").style.display = "none";
+    var locationyoga = document.getElementById("locationyoga").style.display = "none";
+    var venteping = document.getElementById("venteping").style.display = "none";
+    var locationping = document.getElementById("locationping").style.display = "block";
+    var locationvest = document.getElementById("locationvest").style.display = "none";
+    var muscu2 = document.getElementById("truc9").style.opacity = 0.5;
+    var muscu2 = document.getElementById("truc10").style.opacity = 1;
+}
+
+
+function AfficherLocationVest() {
+    var ventemuscu = document.getElementById("ventemuscu").style.display = "none";
+    var locationmuscu = document.getElementById("locationmuscu").style.display = "none";
+    var venteyoga = document.getElementById("venteyoga").style.display = "none";
+    var locationyoga = document.getElementById("locationyoga").style.display = "none";
+    var venteping = document.getElementById("venteping").style.display = "none";
+    var locationping = document.getElementById("locationping").style.display = "none";
+    var locationvest = document.getElementById("locationvest").style.display = "block";
+    var muscu2 = document.getElementById("truc10").style.opacity = 1;
+}
